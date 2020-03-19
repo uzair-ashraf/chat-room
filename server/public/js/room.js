ee34617b4a581db7144f55302bf2336d6d837410
Uzair Ashraf <uzinatorcl@live.com>
2020-03-19 14:27:49 -0700

    user login name is now popping up

--- server/public/js/room.js
+++ server/public/js/room.js
@@ -3,6 +3,12 @@ const room = io.connect('//:3000/rooms');
 
 room.emit('new-user-connected', roomName, username);
 
+const usersContainer = document.querySelector('.connected-users-container');
+
+
 room.on('user-connected', username => {
-  console.log('new user', username);
+  const user = document.createElement('div');
+  user.className = 'user';
+  user.textContent = username;
+  usersContainer.appendChild(user);
 })
