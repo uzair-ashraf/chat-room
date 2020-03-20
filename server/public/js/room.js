d8a9ebf2dd33e109ebbac8ace360a3a2ab7dc31e
Uzair Ashraf <uzinatorcl@live.com>
2020-03-19 19:30:36 -0700

    added notifications

--- server/public/js/room.js
+++ server/public/js/room.js
@@ -9,7 +9,41 @@ const messagesContainer = document.querySelector('.messages-container');
 
 scrollContainerToBottom();
 
+let notificationPermission = null;
+let notificationInProgress = false;
+
+
+const showNotification = async (username, type) => {
+  if(notificationInProgress || !document.hidden) return;
+  if (('Notification' in window)) {
+    if (notificationPermission !== 'denied') {
+        const permission = await Notification.requestPermission();
+        notificationPermission = permission;
+        if(permission === 'granted') {
+          let message = null
+          if(type === 'new-message') {
+            message = `New Message from ${username}!`
+          } else if(type === 'new-user') {
+            message = `${username} has joined your room!`
+          } else {
+            message = `${username} has left your room!`
+          }
+          notificationInProgress = true;
+          const notification = new Notification(message, {
+            icon: '/images/favico.ico'
+          });
+          const closeFunc = notification.close.bind(notification)
+          setTimeout(() => {
+            closeFunc();
+            notificationInProgress = false;
+          }, 5000);
+        }
+      }
+    }
+  }
+
 room.on('user-connected', username => {
+  showNotification(username, 'new-user');
   const user = document.createElement('div');
   user.className = 'user';
   user.textContent = username;
@@ -23,6 +57,7 @@ room.on('user-connected', username => {
 })
 
 room.on('user-disconnected', user => {
+  showNotification(user, 'disconnected-user');
   const usersList = Array.from(usersContainer.children);
   const userElement = usersList.find(u => u.innerText === user);
   userElement.remove();
@@ -49,6 +84,7 @@ messageForm.addEventListener('submit', e => {
 })
 
 function createMessage(name, message) {
+  showNotification(name, 'new-message');
   const messageParent = document.createElement('div');
   const messageSender = document.createElement('span');
   const messageContent = document.createElement('div');
